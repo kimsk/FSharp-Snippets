ecbe88759d5e1e89de901c5ddc91935b5225a3bb
Karlkim Suwamongkol <karlkim@outlook.com>
2014-04-23 20:48:46 -0500

    Try numl Tennis sample

---
+++ TryNumlFSharp/TennisData/Tennis.cs
@@ -0,0 +1,43 @@
+﻿using numl.Model;
+
+namespace TennisData
+{
+    public enum Outlook
+    {
+        Sunny,
+        Overcast,
+        Rainy
+    }
+
+    public enum Temperature
+    {
+        Low,
+        High
+    }
+
+    public class Tennis
+    {
+        [Feature]
+        public Outlook Outlook { get; set; }
+        [Feature]
+        public Temperature Temperature { get; set; }
+        [Feature]
+        public bool Windy { get; set; }
+        [Label]
+        public bool Play { get; set; }
+
+        public static Tennis[] GetData()
+        {
+            return new Tennis[]  {
+                new Tennis { Play = true, Outlook=Outlook.Sunny, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = false, Outlook=Outlook.Sunny, Temperature = Temperature.High, Windy=true},
+                new Tennis { Play = false, Outlook=Outlook.Sunny, Temperature = Temperature.High, Windy=false},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.High, Windy= false},
+                new Tennis { Play = true, Outlook=Outlook.Overcast, Temperature = Temperature.Low, Windy=false},
+                new Tennis { Play = false, Outlook=Outlook.Rainy, Temperature = Temperature.Low, Windy=true},
+                new Tennis { Play = true, Outlook=Outlook.Rainy, Temperature = Temperature.Low, Windy=false}
+            };
+        }
+    }
+}
---
+++ TryNumlFSharp/TryNumlFSharp/Script1.fsx
@@ -0,0 +1,26 @@
+﻿#r @"..\packages\numl.0.7.6\lib\net40\numl.dll"
+#r @"..\TennisData\bin\Debug\TennisData.dll"
+
+open TennisData
+open numl
+open numl.Model
+open numl.Supervised.DecisionTree
+open numl.Supervised.Regression
+let data = Tennis.GetData() |> Seq.cast<obj>
+
+let d = Descriptor.Create<Tennis>()
+let g = DecisionTreeGenerator(d)
+g.SetHint(false)
+
+let model = Learner.Learn(data, 0.8, 1000, g)
+printfn "%A" model
+
+let tennis1 = Tennis(Outlook = Outlook.Sunny, Temperature = Temperature.Low, Windy = false)
+let play1 = model.Model.Predict<Tennis>(tennis1).Play
+
+let tennis2 = Tennis(Outlook = Outlook.Rainy, Temperature = Temperature.Low, Windy = true)
+let play2 = model.Model.Predict<Tennis>(tennis2).Play
+
+printfn "%A" play2
+
+
