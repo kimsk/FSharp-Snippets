57ed8008f815ac90b189fd2d9e295799218a3e8a
Karlkim Suwamongkol <karlkim@outlook.com>
2014-04-24 13:18:08 -0500

    Play with DigitRecognizer data

---
+++ TryNumlFSharp/TennisData/DigitRecognizerRecord.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using numl.Model;
+
+namespace TennisData
+{
+    public enum Digit
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine
+    }
+
+    public class DigitRecognizerRecord
+    {
+        [Label]
+        public Digit Digit { get; set; }
+
+        [EnumerableFeature(50)]
+        public float[] Pixels { get; set; }
+    }
+
+    public static class Stuff
+    {
+        static Random Rng = new Random();
+        public static Dictionary<Digit, float[]> Pixels = new Dictionary<Digit, float[]>(); 
+        static Stuff()
+        {
+            foreach(Digit digit in (Digit[])Enum.GetValues(typeof(Digit)))
+            {
+                Pixels[digit] = getRandomFloats(50);
+            }
+        }
+
+        static float[] getRandomFloats(int num)
+        {            
+            var ret = new List<float>();
+            for (int i = 0; i < num; i++)
+            {
+                ret.Add(Rng.Next(0,255));
+            }
+
+            return ret.ToArray();
+        }
+
+        static DigitRecognizerRecord getRecord(Digit digit)
+        {
+            return new DigitRecognizerRecord {Digit = digit, Pixels = Pixels[digit]};
+        }
+
+        public static IEnumerable<DigitRecognizerRecord> GetRecords()
+        {
+            return new[]
+            {
+                getRecord(Digit.One),
+                getRecord(Digit.Two),
+                getRecord(Digit.Three),
+                getRecord(Digit.One),
+                getRecord(Digit.Four),
+                getRecord(Digit.Five),
+                getRecord(Digit.One),
+                getRecord(Digit.Eight),
+                getRecord(Digit.One),
+                getRecord(Digit.Nine),
+                getRecord(Digit.Four),
+                getRecord(Digit.Seven),
+                getRecord(Digit.Seven),
+                getRecord(Digit.Eight),
+                getRecord(Digit.Six),
+            };          
+        } 
+    }
+}
---
+++ TryNumlFSharp/TryNumlFSharp/DigitRecognizer.fsx
@@ -0,0 +1,46 @@
+﻿#r @"..\packages\numl.0.7.6\lib\net40\numl.dll"
+#r @"..\TennisData\bin\Debug\TennisData.dll"
+
+open TennisData
+open numl
+open numl.Model
+open numl.Supervised
+open numl.Supervised.DecisionTree
+open numl.Supervised.NaiveBayes
+let data = Stuff.GetRecords() |> Seq.cast<obj>
+
+let d = Descriptor.Create<DigitRecognizerRecord>()
+
+let getDecisionTreeModel description (data:seq<obj>) =
+    let g = DecisionTreeGenerator(description)
+    g.SetHint(false)
+    let model = Learner.Learn(data, 0.8, 1000, g).Model
+    printfn "%A" model
+    model
+
+let getNaiveBayesModel description (data:seq<obj>) =
+    let g = NaiveBayesGenerator(2)
+    g.Generate(description, data)
+
+let predict (model:IModel) pixels =
+    let r = DigitRecognizerRecord(Pixels = pixels)
+    model.Predict<DigitRecognizerRecord>(r).Digit
+
+let dtModel = getDecisionTreeModel d data
+let nbModel = getNaiveBayesModel d data
+
+let tests = [|
+    Stuff.Pixels.[Digit.One]
+    Stuff.Pixels.[Digit.Two]
+    Stuff.Pixels.[Digit.Three]
+    Stuff.Pixels.[Digit.Four]
+    Stuff.Pixels.[Digit.Five]
+    Stuff.Pixels.[Digit.Six]
+    Stuff.Pixels.[Digit.Seven]
+    Stuff.Pixels.[Digit.Eight]
+    Stuff.Pixels.[Digit.Nine]
+    Stuff.Pixels.[Digit.Six]
+|] 
+
+tests |> Array.map (fun d -> predict dtModel d)
+tests|> Array.map (fun d -> predict nbModel d)
