b8b7461268cf3ccbba4fa454c10058636eb9a5ad
Karlkim Suwamongkol <karlkim@outlook.com>
2014-04-26 23:08:28 -0500

    Add a file to test the CSV Type Provider

---
+++ kaggle/facial-keypoints-detection/FacialKeyPointsDetection/FacialKeyPointsDetection/TestCsvProvider.fsx
@@ -0,0 +1,18 @@
+﻿#I @"..\packages\FsLab.0.0.13-beta\"
+#load @"FsLab.fsx"
+
+open FSharp.Data
+open System.IO
+
+type FaceCsv = CsvProvider<"D:/kaggle/facial-keypoints-detection/training.csv", InferRows=0, PreferOptionals = true>
+let faces = new FaceCsv()
+
+(faces.Rows |> Seq.nth 1585).left_eyebrow_outer_end_x
+
+let lines = File.ReadAllLines("D:/kaggle/facial-keypoints-detection/training.csv")
+
+
+(lines |> Seq.nth 0).Split(',') |> Seq.iteri (fun i r -> printfn "%d %s" i r)
+(lines |> Seq.nth 1586).Split(',') |> Seq.iteri (fun i r -> printfn "%d %s" i r)
+
+(lines |> Seq.nth 1586)
