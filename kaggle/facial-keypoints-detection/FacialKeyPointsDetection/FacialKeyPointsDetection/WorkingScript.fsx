1fa5b364dce30bef8e0a0aef14612dd6c369c607
Karlkim Suwamongkol <karlkim@outlook.com>
2014-04-29 22:22:06 -0500

    Using Math.NET is easier and faster :-)

--- kaggle/facial-keypoints-detection/FacialKeyPointsDetection/FacialKeyPointsDetection/WorkingScript.fsx
+++ kaggle/facial-keypoints-detection/FacialKeyPointsDetection/FacialKeyPointsDetection/WorkingScript.fsx
@@ -7,6 +7,15 @@ open RProviderConverters
 open System
 open System.IO
 
+// R Stuff
+open RProvider
+open RProvider.``base``
+open RProvider.graphics
+open RProvider.grDevices
+open RDotNet
+
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
 
 type Face =
     {
@@ -64,23 +73,9 @@ let faces =
             }
         )
 
-faces |> Seq.skip 210 |> Seq.iteri (fun i r -> printfn "%d %A" i r)
-((lines
-|> Seq.nth 211).Split(',')
-|> Seq.nth 30).Split(' ') |> Array.iteri (fun i r -> printfn "%d %d" i (Int32.Parse(r)))
-
-
-// R Stuff
-open RProvider
-open RProvider.``base``
-open RProvider.graphics
-open RProvider.grDevices
-open RDotNet
-
-// helper functions
+let face i = faces |> Seq.nth i
 let o x = x :> obj
 let imXy p = match p with Some(x,y) -> (96m-x),(96m-y) | _ -> (0m,0m)     
-let face i = faces |> Seq.nth i
 let imPointParams (x,y) color = ["x",o x;"y",o y;"col",o color] |> namedParams
 
 // Visualize image using R
@@ -93,10 +88,6 @@ let grayScaleImage nrow ncol data =
         "col", gray
     ] |> namedParams |> R.image
 
-// helper function to slice matrix
-let sliceMatrix (m:SymbolicExpression) (x1,x2,y1,y2) =
-    m.Engine.SetSymbol("m", m)
-    sprintf "m[%M:%M,%M:%M]" x1 x2 y1 y2 |> m.Engine.Evaluate
 
 // image(1:96, 1:96, im, col=gray((0:255)/255))
 Array.rev((face 0).Image) |> grayScaleImage 96 96
@@ -113,83 +104,7 @@ R.points(imPointParams rightEyeCenterXy "green")
 // add all nose points
 seq { for f in faces -> f } |> Seq.iter (fun f -> R.points(imPointParams (imXy f.NoseTip) "red") |> ignore)
 
-// Get R Data Frame
-let facesR_Df =    
-    let d x =
-        match Double.TryParse x with
-        | true, v -> v
-        | _ -> Double.NaN
-
-    let lines = lines |> Seq.map(fun (r:string) -> r.Split(','))
-    lines 
-    |> Seq.nth 0 
-    |> Seq.mapi (fun i h -> h, lines |> Seq.skip 1 |> Seq.map (fun r -> d r.[i]))
-    |> (namedParams >> R.data_frame)
- 
-R.colMeans(facesR_Df, na_rm=true)
-
-let v = R.c([|1..400|])
-let m = R.matrix(data=v, nrow=20,ncol=20)
-R.colMeans(m)
-
-
-#time
-let facesDeedleDf =
-    let d x =
-        match Double.TryParse x with
-        | true, v -> v
-        | _ -> Double.NaN
-
-    let lines = lines |> Seq.map(fun (r:string) -> r.Split(','))
-    lines 
-    |> Seq.nth 0 
-    |> Seq.mapi (fun i h -> h, lines |> Seq.skip 1 |> Seq.map (fun r -> d r.[i]) |> Series.ofValues)
-    |> Frame.ofColumns
-#time
-// deedle data frame can be used with R!
-let dv:float[] = R.colMeans(facesDeedleDf, na_rm=true).GetValue()
-facesDeedleDf?left_eye_center_x
-facesDeedleDf.Columns
-facesDeedleDf.Rows
-facesDeedleDf.Rows.[10]
-
-
 let patchSize = 10m
-// too slow...
-let patches = 
-    faces 
-    |> Seq.filter (fun f -> f.LeftEyeCenter.IsSome) 
-    |> Seq.map (fun f -> f.LeftEyeCenter.Value)
-    |> Seq.mapi (fun i (x,y) -> 
-            let im = R.matrix(nrow=96,ncol=96,data=(face i).Image)
-            let x1,x2,y1,y2 = (x-patchSize),(x+patchSize),(y-patchSize),(y+patchSize)
-            if x1>=1m && x2<=96m && y1>=1m && y2 <=96m then
-                let newMatrix = sliceMatrix im (x1,x2,y1,y2)
-                Some(newMatrix)
-            else
-                None                
-        )
-    |> Seq.filter (fun p -> p.IsSome)
-    |> Seq.map (fun p -> p.Value)
-
-// show patch of eye
-R.rev(x=(patches |> Seq.nth 2 |> R.as_vector)) |> grayScaleImage 21 21
-
-// calculate means
-#time
-let p10 = patches |> Seq.take 1000 |> Seq.map (R.as_vector) |> Seq.collect (fun p -> p.AsNumeric() |> Array.ofSeq)
-let m10 = R.matrix(data=p10,nrow=1000,ncol=441)
-#time
-let means = R.colMeans(m10)
-    
-R.rev(means) |> grayScaleImage 21 21 
-
-open MathNet.Numerics.LinearAlgebra
-open MathNet.Numerics.LinearAlgebra.Double
-
-let slice r1 r2 r3 r4 (m:Matrix<float>) = 
-    m.GetSlice(Some(r1), Some(r2), Some(r3), Some(r4))
-
 let slicedMatrices = 
     faces 
     |> Seq.filter (fun f -> f.LeftEyeCenter.IsSome) 
@@ -204,7 +119,8 @@ let slicedMatrices =
         ) 
     |> Seq.cache
 
-slicedMatrices |> Seq.length
+let vc = R.c([0..20] |> Seq.collect (fun i -> (slicedMatrices |> Seq.nth 7032).Column(i)) |> Array.ofSeq |> Array.rev)
+vc |> grayScaleImage 21 21
 
 #time
 let means2 = 
@@ -219,5 +135,4 @@ let means2 =
 let meansVc = R.c([0..20] |> Seq.collect (fun i -> means2.Column(i)) |> Array.ofSeq |> Array.rev)
 meansVc |> grayScaleImage 21 21
 
-let vc = R.c([0..20] |> Seq.collect (fun i -> (slicedMatrices |> Seq.nth 7032).Column(i)) |> Array.ofSeq |> Array.rev)
-vc |> grayScaleImage 21 21
+let searchSize = 2
