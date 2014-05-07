ef3a586d465b3e5503e0d034f830ef2e6b38aeb1
Karlkim Suwamongkol <karlkim@outlook.com>
2014-05-07 12:10:01 -0500

    refactor code

--- udacity/intro-to-statistics/FsLab Notebook/FsLab Notebook/ScatterPlot.fsx
+++ udacity/intro-to-statistics/FsLab Notebook/FsLab Notebook/ScatterPlot.fsx
@@ -8,24 +8,17 @@ Scatter Plots
 
 F# code for [Scatter Plots] unit.
 *)
-[<Measure>] type sqrft
+[<Measure>] type sqft
 [<Measure>] type dollar
-let data1 = 
-    [
-        (1700<sqrft>,51000<dollar>)
-        (2100<sqrft>,63000<dollar>)
-        (1900<sqrft>,57000<dollar>)
-        (1300<sqrft>,39000<dollar>)
-        (1600<sqrft>,48000<dollar>)
-        (2200<sqrft>,66000<dollar>)
-    ]
+let sizes = [1700<sqft>;2100<sqft>;1900<sqft>;1300<sqft>;1600<sqft>;2200<sqft>]
+let prices1 = [51000<dollar>;63000<dollar>;57000<dollar>;39000<dollar>;48000<dollar>;66000<dollar>]
 
 open Deedle
-let df1 = Frame(["size"; "price"], [ data1 |> Seq.map fst |> Series.ofValues ; data1 |> Seq.map snd |> Series.ofValues])
+let df1 = Frame(["size"; "price"], [ Series.ofValues sizes; Series.ofValues prices1])
 (*** include-value:df1 ***)
 (*** define-output:chart1 ***)
 open FSharp.Charting
-Chart.Point(data1)
+Chart.Point(Seq.zip sizes prices1)
 (*** include-it:chart1 ***)
 (**
 Is this linear? **Yes**
@@ -33,25 +26,16 @@ Is this linear? **Yes**
 (**
 Do we believe there is a fixed price per square foot? **Yes**
 *)
-let pricePerSqrft1 = (data1 |> Seq.map snd |> Seq.sum)/(data1 |> Seq.map fst |> Seq.sum)
+let pricePerSqrft1 = (Seq.sum prices1)/(Seq.sum sizes)
 (*** include-value:pricePerSqrft1 ***)
 (** 
 ** Second data set
 *)
-let data2 = 
-    [
-        (1700<sqrft>,53000<dollar>)
-        (2100<sqrft>,65000<dollar>)
-        (1900<sqrft>,59000<dollar>)
-        (1300<sqrft>,41000<dollar>)
-        (1600<sqrft>,50000<dollar>)
-        (2200<sqrft>,68000<dollar>)
-    ]
-
-let df2 = Frame(["size"; "price"], [ data2 |> Seq.map fst |> Series.ofValues ; data2 |> Seq.map snd |> Series.ofValues])
+let prices2 = [53000<dollar>;65000<dollar>;59000<dollar>;41000<dollar>;50000<dollar>;68000<dollar>]
+let df2 = Frame(["size"; "price"], [ Series.ofValues sizes; Series.ofValues prices2])
 (*** include-value:df2 ***)
 (*** define-output:chart2 ***)
-Chart.Point(data2)
+Chart.Point(Seq.zip sizes prices2)
 (*** include-it:chart2 ***)
 (**
 Is this linear? **Yes**
@@ -59,31 +43,23 @@ Is this linear? **Yes**
 (**
 Do we believe there is a fixed price per square foot? **No**
 *)
-let pricePerSqrft2 = float(data2 |> Seq.map snd |> Seq.sum)/float(data2 |> Seq.map fst |> Seq.sum)
+let pricePerSqrft2 = float(Seq.sum prices2)/float(Seq.sum sizes)
 (*** include-value:pricePerSqrft2 ***)
-let firstHome = pricePerSqrft2*1700.<sqrft>
+let firstHome = pricePerSqrft2*1700.<sqft>
 (*** include-value:firstHome***)
 (**
 How to calculate the price?
 *)
-30<dollar/sqrft>*1700<sqrft> + 2000<dollar>
+30<dollar/sqft>*1700<sqft> + 2000<dollar>
 (** 
 ** 3rd data set
 *)
-let data3 = 
-    [
-        (1700<sqrft>,53000<dollar>)
-        (2100<sqrft>,44000<dollar>)
-        (1900<sqrft>,59000<dollar>)
-        (1300<sqrft>,82000<dollar>)
-        (1600<sqrft>,50000<dollar>)
-        (2200<sqrft>,68000<dollar>)
-    ]
+let prices3 = [53000<dollar>;44000<dollar>;59000<dollar>;82000<dollar>;50000<dollar>;68000<dollar>]
 
-let df3 = Frame(["size"; "price"], [ data3 |> Seq.map fst |> Series.ofValues ; data3 |> Seq.map snd |> Series.ofValues])
+let df3 = Frame(["size"; "price"],  [ Series.ofValues sizes; Series.ofValues prices3])
 (*** include-value:df3 ***)
 (*** define-output:chart3 ***)
-Chart.Point(data3)
+Chart.Point(Seq.zip sizes prices3)
 (*** include-it:chart3 ***)
 (**
 Is this linear? **No**, because there are [Outlier]s
