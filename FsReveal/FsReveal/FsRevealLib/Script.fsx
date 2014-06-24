d84c08fa4e1a4fc7a6369fa14c334c39d3442410
Karlkim Suwamongkol <karlkim@outlook.com>
2014-06-23 22:06:19 -0500

    Move FsReveal to fs file

--- FsReveal/FsReveal/FsRevealLib/Script.fsx
+++ FsReveal/FsReveal/FsRevealLib/Script.fsx
@@ -1,7 +1,6 @@
 ﻿//#I "../packages/FSharp.Formatting.2.4.10/lib/net40/"
-#I @"C:\Users\Karlkim\Documents\GitHub\FSharp.Formatting\bin"
-#I "../packages/RazorEngine.3.3.0/lib/net40"
-#I "../packages/FSharp.Compiler.Service.0.0.44/lib/net40"
+//#I @"C:\Users\Karlkim\Documents\GitHub\FSharp.Formatting\bin"
+#I @"G:\GitHub\FSharp.Formatting\bin"
 #r "FSharp.Literate.dll"
 #r "FSharp.CodeFormat.dll"
 #r "FSharp.MetadataFormat.dll"
@@ -10,6 +9,7 @@
 #r "FSharp.Compiler.Service.dll"
 #r "CSharpFormat.dll"
 
+#load "FsReveal.fs"
 
 open FSharp.Literate
 open FSharp.Markdown
@@ -19,103 +19,24 @@ open CSharpFormat
 open System
 open System.IO
 open System.Web
-
-type FsReveal() =
-
-  static let formattingContext templateFile format prefix lineNumbers includeSource replacements layoutRoots =
-      { TemplateFile = templateFile 
-        Replacements = defaultArg replacements []
-        GenerateLineNumbers = defaultArg lineNumbers true
-        IncludeSource = defaultArg includeSource false
-        Prefix = defaultArg prefix "fs"
-        OutputKind = defaultArg format OutputKind.Html
-        LayoutRoots = defaultArg layoutRoots [] }
-
-  static let rec collectCodes par = seq {
-      match par with 
-      | Matching.LiterateParagraph(HiddenCode(Some id, lines)) -> 
-          yield Choice2Of2(id), lines
-      | Matching.LiterateParagraph(DoNotEvalCode(lines))
-      | Matching.LiterateParagraph(NamedCode(_,lines))
-      | Matching.LiterateParagraph(FormattedCode(lines)) ->         
-          yield Choice1Of2(lines), lines
-      | Matching.ParagraphNested(pn, nested) ->
-          yield! Seq.collect (Seq.collect collectCodes) nested
-      | _ -> () }
-
-  static let rec replaceSpecialCodes ctx (formatted:IDictionary<_, _>) = function
-    | Matching.LiterateParagraph(special) -> 
-        match special with
-        | HiddenCode _ -> None
-        | CodeReference ref -> Some (formatted.[Choice2Of2 ref])
-        | OutputReference _  
-        | ItValueReference _  
-        | ValueReference _ -> 
-            failwith "Output, it-value and value references should be replaced by FSI evaluator"
-        | DoNotEvalCode lines
-        | NamedCode(_,lines)
-        | FormattedCode lines -> Some (formatted.[Choice1Of2 lines])      
-        | LanguageTaggedCode(lang, code) -> 
-            let inlined = 
-              match ctx.OutputKind with
-              | OutputKind.Html ->
-                  let code = HttpUtility.HtmlEncode code
-                  let code = SyntaxHighlighter.FormatCode(lang, code)
-                  sprintf "<pre lang=\"%s\">%s</pre>" lang code
-              | OutputKind.Latex ->
-                  sprintf "\\begin{lstlisting}\n%s\n\\end{lstlisting}" code
-            Some(InlineBlock(inlined))
-        | StartSlide -> Some(StartSlideBlock)
-        | EndSlide -> Some(EndSlideBlock)
-    // Traverse all other structures recursively
-    | Matching.ParagraphNested(pn, nested) ->
-        let nested = List.map (List.choose (replaceSpecialCodes ctx formatted)) nested
-        Some(Matching.ParagraphNested(pn, nested))
-    | par -> Some par
-
-  static let replaceLiterateParagraphs ctx (doc:LiterateDocument) = 
-      let replacements = Seq.collect collectCodes doc.Paragraphs
-      let snippets = [| for _, r in replacements -> Snippet("", r) |]    
-    
-      // Format all snippets and build lookup dictionary for replacements
-      let formatted = CodeFormat.FormatHtml(snippets, ctx.Prefix, ctx.GenerateLineNumbers, false)
-      
-      let lookup = 
-        [ for (key, _), fmtd in Seq.zip replacements formatted.Snippets -> 
-            key, InlineBlock(fmtd.Content) ] |> dict 
-   
-    
-      // Replace original snippets with formatted HTML and return document
-      let newParagraphs = List.choose (replaceSpecialCodes ctx lookup) doc.Paragraphs
-      doc.With(paragraphs = newParagraphs, formattedTips = formatted.ToolTip)
-
-  static member GetHtmlWithoutFormattedTips(fsx, ?prefix, ?lineNumbers) =
-    let doc = fsx |> Literate.ParseScriptString
-
-    let ctx = formattingContext None (Some OutputKind.Html) prefix lineNumbers None None None
-    let doc = replaceLiterateParagraphs ctx doc
-    Markdown.WriteHtml(MarkdownDocument(doc.Paragraphs, doc.DefinedLinks))
-
-  static member GetHtmlFormattedTips fsx =
-    let slides = fsx  |> Literate.ParseScriptString
-    let replacements = Seq.collect collectCodes slides.Paragraphs
-    let snippets = [| for _, r in replacements -> Snippet("", r) |]       
-    
-    let ctx = formattingContext None (Some OutputKind.Html) None None None None None
-    let formatted = CodeFormat.FormatHtml(snippets, ctx.Prefix, ctx.GenerateLineNumbers, false)    
-    formatted.ToolTip    
-
+open FsRevealLib
 
 let fsx ="""
+(*** slide-start ***)
 (*** slide-start ***)
 (**
 ## F# syntax in 60 seconds
 
 ### The two major differences between F# syntax and a standard C-like syntax are:
-
+*)
+(*** slide-end ***)
+(*** slide-start ***)
+(**
 - Curly braces are not used to delimit blocks of code. Instead, indentation is used (Python is similar this way).
 - Whitespace is used to separate parameters rather than commas.
 *)
+(*** slide-end ***)
+(*** slide-start ***)
 // The "let" keyword defines an (immutable) value
 let myInt = 5
 let myFloat = 3.14
@@ -127,6 +48,7 @@ let twoToFive = [2;3;4;5]        // Square brackets create a list with
 let oneToFive = 1 :: twoToFive   // :: creates list with new 1st element
 // The result is [1;2;3;4;5]
 let zeroToFive = [0;1] @ twoToFive   // @ concats two lists
+(*** slide-end ***)
 (*** slide-end ***)
 (*** slide-start ***)
 (**
@@ -144,5 +66,11 @@ let sequence = seq {1..10}
 (*** slide-end ***)
 """
 
-let html = fsx |> FsReveal.GetHtmlWithoutFormattedTips
-let tips = fsx |> FsReveal.GetHtmlFormattedTips
+let slides,tooltips = fsx |> FsReveal.GetHtmlWithoutFormattedTips
+
+let relative subdir = Path.Combine(__SOURCE_DIRECTORY__, subdir)
+
+let template = File.ReadAllText (relative "template.html")
+
+let output = 
+  template.Replace("{tooltips}", tooltips).Replace("{slides}", slides)
