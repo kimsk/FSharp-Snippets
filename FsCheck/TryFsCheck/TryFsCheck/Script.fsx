8cc3f5af6780d674f7677106bb3469c5e9eb1c3e
Karlkim Suwamongkol <karlkim@outlook.com>
2014-07-10 12:54:29 -0500

    Try FsCheck

---
+++ FsCheck/TryFsCheck/TryFsCheck/Script.fsx
@@ -0,0 +1,15 @@
+﻿#r @"..\packages\FsCheck.0.9.4.0\lib\net40-Client\FsCheck.dll"
+#r @"..\packages\NUnit.2.6.3\lib\nunit.framework.dll"
+#r @"..\packages\FsUnit.1.3.0.1\Lib\Net40\FsUnit.NUnit.dll"
+open FsUnit
+open FsCheck
+
+Check.QuickThrowOnFailure(fun (x:string) -> 
+    x <> null ==> lazy
+        x.Length
+        |> should greaterThan -1
+)
+
+Check.QuickThrowOnFailure(fun a b c-> 
+    ((a * b) * c )|> should equal (a * (b * c))
+)
