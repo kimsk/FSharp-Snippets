0c714e3cec8096d48f956c1f2fbbfebe6b4c34d8
Karlkim Suwamongkol <karlkim@outlook.com>
2014-07-14 17:05:00 -0500

    FsReveal test scripts

---
+++ FsReveal/scripts/Script.fsx
@@ -0,0 +1,29 @@
+﻿#I @"..\..\..\FsReveal\src\FsReveal\bin"
+
+#r "FSharp.Literate.dll"
+#r "FSharp.Markdown.dll"
+
+#load @"..\..\..\FsReveal\src\FsReveal\FsReveal.fs"
+
+open System.IO
+open FsReveal
+open FSharp.Literate
+open FSharp.Markdown
+
+[1;2;3;0;1;2;3] |> splitBy 0
+
+let fsxLocation = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.fsx")
+let fsx = File.ReadAllText (fsxLocation)
+getPresentationFromScriptString fsx
+
+let mdLocation = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.md")
+let md = File.ReadAllText (mdLocation)
+let presentation = getPresentationFromMarkdown md
+
+
+// should be true
+getPresentationFromMarkdown md = getPresentationFromScriptString fsx
+
+
+
+
