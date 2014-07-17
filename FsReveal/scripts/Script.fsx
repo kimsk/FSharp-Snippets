ce5a4833dd41bb5e1ac1c1e12beae9f6c99067f8
Karlkim Suwanmongkol <contact@karlk.im>
2014-07-17 12:09:59 -0500

    Use FSharp.Formatting NuGet

--- FsReveal/scripts/Script.fsx
+++ FsReveal/scripts/Script.fsx
@@ -1,5 +1,4 @@
-﻿//#I @"..\..\..\FsReveal\src\packages\FSharp.Formatting.2.4.14\lib\net40"
-#I @"..\..\..\FSharp.Formatting\bin"
+﻿#I @"..\..\..\FsReveal\src\packages\FSharp.Formatting.2.4.18\lib\net40"
 
 #r "FSharp.Literate.dll"
 #r "FSharp.Markdown.dll"
