15e753af738963967549b777dbfce75e5a3a93cc
Karlkim Suwamongkol <contact@karlk.im>
2014-07-21 21:07:00 -0500

    Clean script.fsx

--- FsReveal/scripts/Script.fsx
+++ FsReveal/scripts/Script.fsx
@@ -1,15 +1,6 @@
-﻿#I @"..\..\..\FsReveal\src\packages\FSharp.Formatting.2.4.18\lib\net40"
-#r "FSharp.Literate.dll"
-#r "FSharp.Markdown.dll"
-#r "FSharp.CodeFormat.dll"
-#r @"..\..\..\FsReveal\src\FsReveal\bin\FsReveal.dll"
+﻿#r @"..\..\..\FsReveal\src\FsReveal\bin\FsReveal.dll"
 
-open System
 open System.IO
-open System.Collections.Generic
-open FSharp.Literate
-open FSharp.Markdown
-open FSharp.Markdown.Html
 
 let outDir = @"g:\output"
 
