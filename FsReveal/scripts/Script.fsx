06dc654d2e2dc27d7231ac2b3b3faf9eca7b10a7
Karlkim Suwamongkol <contact@karlk.im>
2014-07-21 20:33:02 -0500

    Update script.fsx

--- FsReveal/scripts/Script.fsx
+++ FsReveal/scripts/Script.fsx
@@ -11,18 +11,10 @@ open FSharp.Literate
 open FSharp.Markdown
 open FSharp.Markdown.Html
 
-let fsxLocation = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.fsx")
-let fsx = File.ReadAllText (fsxLocation)
-
-let mdLocation = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.md")
-let md = File.ReadAllText (mdLocation)
-
 let outDir = Path.Combine(__SOURCE_DIRECTORY__, "../reveal.js")
 
-fsx 
-|> FsReveal.getPresentationFromScriptString
-|> FsReveal.generateOutput outDir "test-fsx.html"
+let fsxFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.fsx")
+FsReveal.generateOutputFromScriptFile outDir "test-fsx.html" fsxFile
 
-md
-|> FsReveal.getPresentationFromMarkdown
-|> FsReveal.generateOutput outDir "test-md.html"
+let mdFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.md")
+FsReveal.generateOutputFromMarkdownFile outDir "test-md.html" mdFile
