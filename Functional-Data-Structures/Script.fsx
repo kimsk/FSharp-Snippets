b0631d5a8510577a0991b4f60258426192eef98e
Karlkim Suwamongkol <contact@karlk.im>
2014-09-07 13:40:02 -0500

    Start Seq.groupBy

--- Functional-Data-Structures/Script.fsx
+++ Functional-Data-Structures/Script.fsx
@@ -1,6 +1,5 @@
 ﻿let b:int[] = Array.zeroCreate 20
 
-
 5
 |> function
    | 1 -> Some(1)
@@ -24,12 +23,17 @@
 [1;2;3;4]
 |> List.reduce (fun acc v -> acc + v)
 
-
+// https://twitter.com/fsibot/status/508465436346691584
 [|for s in "♣♦♥♠" do 
     for r in "A23456789TJQK" -> 
         System.String[|r;s|]|] 
     |> Array.sortBy (fun _ -> System.Random().NextDouble())
 
+
+Array.choose
+
+
+// generate sequence
 let s1 = seq {1..1000}
 let s2 = {1..1000}
 let s3 = seq {for i in 1..1000 -> i}
@@ -37,10 +41,20 @@ let s4 = Seq.initInfinite (fun i -> i * i)
 s4 |> Seq.take 1000 |> Seq.nth 0
 
 
-2L |> Seq.unfold (fun i -> Some(i, i * i)) |> Seq.take 6 |> Array.ofSeq 
-
-let fizzBuzz = 1|>Seq.unfold(fun i -> match i with i when i%3=0&&i%5=0->Some("FizzBuzz",i+1)|i when i%3=0->Some("Fizz",i+1)|i when i%5=0->Some("Buzz",i+1)|_->Some(i.ToString(),i+1))
+let fizzBuzz = 
+  1
+  |>Seq.unfold(fun i -> 
+    match i%3,i%5 with 
+    | 0,0 -> Some("FizzBuzz", i+1)
+    | 0,_ -> Some("Fizz",i+1)
+    | _,0 -> Some("Buzz",i+1)
+    | _ -> Some(i.ToString(),i+1))
 fizzBuzz |> Seq.take 20 |> Array.ofSeq
+
+// https://twitter.com/fsibot/status/508661333030486016
+Seq.unfold(fun i->match i%3,i%5 with 0,0->Some("F#", i+1)|0,_->Some("F",i+1)|_,0->Some("#",i+1)|_-> Some(i.ToString(),i+1)) 12
+let s(v,i)= Some(v,i)
+Seq.unfold(fun i->match i%3,i%5 with 0,0->s("FizzBuzz",i+1)|0,_->s("Fizz",i+1)|_,0->s("Buzz",i+1)|_->s(string(i),i+1)) 1
 
 open System
 let DaysFollowing (start:DateTime) =
@@ -52,7 +66,38 @@ DaysFollowing (DateTime(2015,2,1)) |> Seq.take 5
 
 "abcd".[0]
 
+// generate sequence using unfold
+2L 
+|> Seq.unfold (fun i -> Some(i, i * i)) 
+|> Seq.take 6 
+|> Array.ofSeq 
+
+// generate sequence using sequence expression with recursion
 let rec seqWithRecursion (i:int64) =
-    seq { yield i;yield! (seqWithRecursion (i*i))}
+    seq { 
+      yield i
+      yield! (seqWithRecursion (i*i))
+    }
 
 seqWithRecursion 2L |> Seq.truncate 6 |> Seq.length 
+
+seq {1..10} |> Seq.find((=)10)
+seq {1..10} |> Seq.tryFind((=)22)
+seq {1..10} |> Seq.pick(fun i -> match i with 10 -> Some(i) | _ -> None)
+seq {1..10} |> Seq.pick(fun i -> match i with 22 -> Some(i) | _ -> None)
+seq {1..10} |> Seq.tryPick(fun i -> match i with 22 -> Some(i) | _ -> None)
+seq {1..10} |> Seq.tryPick(fun i -> match i with 10 -> Some(i) | _ -> None)
+
+let rnd = new System.Random()
+Seq.initInfinite (fun _ -> rnd.Next())
+
+let isPrime n =
+  printfn "isPrime %d" n
+  let rec check i = 
+    printfn "check %d" i
+    i > n/2 || (n%i <> 0 && check (i+1))
+  check 2
+
+Seq.initInfinite (fun _ -> rnd.Next(1, 1000))
+|> Seq.take 10
+|> Seq.map (isPrime)
