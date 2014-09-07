c100bbec330b660d7fe6c0fa7f30f7b6da9631c7
Karlkim Suwamongkol <contact@karlk.im>
2014-09-07 11:51:22 -0500

    update fsreveal script (forgot)

--- FsReveal/scripts/Script.fsx
+++ FsReveal/scripts/Script.fsx
@@ -1,11 +1,12 @@
-﻿#r @"..\..\..\FsReveal\src\FsReveal\bin\FsReveal.dll"
+﻿#r @"..\..\..\FsReveal\bin\FsReveal.dll"
 
 open System.IO
+open FsReveal
 
 let outDir = @"g:\output"
 
-let fsxFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.fsx")
-FsReveal.generateOutputFromScriptFile outDir "test-fsx.html" fsxFile
+let fsxFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\src\presentations\FsReveal.fsx")
+FsReveal.GenerateOutputFromScriptFile outDir "test-fsx.html" fsxFile
 
-let mdFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\presentations\FsReveal.md")
-FsReveal.generateOutputFromMarkdownFile outDir "test-md.html" mdFile
+let mdFile = Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\FsReveal\src\presentations\FsReveal.md")
+FsReveal.GenerateOutputFromMarkdownFile outDir "test-md.html" mdFile
