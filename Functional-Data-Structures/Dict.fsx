3f886c928cc3a39224fe9dcf8c9b5cdd0982316d
Karlkim Suwamongkol <contact@karlk.im>
2014-09-13 21:37:54 -0500

    merge changes from vm

---
+++ Functional-Data-Structures/Dict.fsx
@@ -0,0 +1,19 @@
+﻿// mutable style
+open System.Collections.Generic
+let dict1 = new Dictionary<string, string>()
+dict1.Add("A","1")
+dict1.["A"] <- "10"
+
+
+// immutable style
+let dict' = [ "A",1;"B",2;"C",3;"C",4] |> dict // undocumented, 
+
+dict'.["A"] // <- 10
+dict'.Add("D",4) // NotSupportedException
+
+dict'.["C"]
+
+
+open System.Collections.Concurrent
+
+let conDict = new ConcurrentDictionary<string, int>()
---
+++ computation-expressions/Script.fsx
@@ -0,0 +1,45 @@
+﻿type Result = Success of float | DivByZero
+
+let divide x y =
+  match y with
+  | 0. -> DivByZero
+  | y -> Success(x/y)
+
+type DefinedBuilder() =
+  member __.Bind(x:Result, rest:float->Result) =
+      match x with
+      | DivByZero -> DivByZero
+      | Success(x) -> rest x
+
+  member __.Return(x) = x
+
+let totalResistance r1 r2 r3 =
+  let defined = DefinedBuilder()
+  defined {
+    let! x = divide 1. r1
+    let! y = divide 1. r2
+    let! z = divide 1. r3
+    return divide 1. (x + y + z)
+  }
+
+
+let b = match Success(1.) with Success(x) -> x | _ -> 0.
+
+let (Success z) = Success(1.)
+
+type LoggingBuilder() =
+  member __.Bind(x,f) =
+    printfn "%s" x
+    f x
+
+  member __.Zero() =     
+    ()
+
+let logging = LoggingBuilder()
+
+logging {
+  let! _ = "A"
+  let! _ = "B"
+  let! _ = "C"
+  ()
+}
