247f2133c15354d6705e745bac96a5845489cc5d
Karlkim Suwamongkol <contact@karlk.im>
2014-09-13 20:02:16 -0500

    FsUnit Workflow

---
+++ FsUnit/ConsoleFSharp/ConsoleFSharp/Program.fs
@@ -0,0 +1,15 @@
+﻿open NUnit.Framework
+open FsUnit
+
+module Calculator = 
+  let add x y = x + y
+  let mul x y = x * y
+
+module Tests =
+  [<Test>]
+  let ``add should work``() =
+    Calculator.add 10 10 |> should equal 20
+
+  [<Test>]
+  let ``mul should work``() =
+    Calculator.mul 10 10 |> should equal 100
