a4cf500b516c935b77d3069a126f3ffbc9d96de9
Karlkim Suwamongkol <contact@karlk.im>
2014-10-12 11:56:50 -0500

    add find middle

---
+++ algorithm/Find-Middle.fsx
@@ -0,0 +1,28 @@
+﻿let findMiddle list =
+    let rec loop fst snd =
+        let second = 
+                match snd with
+                | a::b::tl ->
+                    tl
+                | _ ->
+                    []
+        let first =
+                match fst with
+                | a::tl ->
+                    tl
+                | _ ->
+                    []
+        printfn "%A %A" first second
+        match fst,second with
+        | hd::tl, [] -> Some(hd)
+        | [],[] -> None
+        | _ -> loop first second
+    loop list list
+
+
+[1;2;3] |> findMiddle
+[1;2;3;4;5] |> findMiddle
+[1;2;3;4;5;6] |> findMiddle
+[1;2;3;4;5;6;7] |> findMiddle
+let out:int option = [] |> findMiddle
+
