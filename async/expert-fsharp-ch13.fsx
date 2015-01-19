52a96d6e76b91f72512f186f2f7bacac4e73c0b5
Karlkim Suwamongkol <contact@karlk.im>
2015-01-19 00:07:03 -0600

    more async

--- async/expert-fsharp-ch13.fsx
+++ async/expert-fsharp-ch13.fsx
@@ -2,6 +2,10 @@
 open System.IO
 open Microsoft.FSharp.Control.WebExtensions
 
+let tprintfn fmt =
+  printf "[.NET thread %d]" System.Threading.Thread.CurrentThread.ManagedThreadId
+  printfn fmt
+
 let links = 
   [
     "google", "http://google.com"
@@ -11,21 +15,57 @@ let links =
 
 let fetchAsync(nm, url:string) =
   async{
-    do printfn "Creating request for %s..." nm
+    do tprintfn "Creating request for %s..." nm
     let req = WebRequest.Create(url)
 
     let! resp = req.AsyncGetResponse()
 
-    do printfn "Getting response stream for %s..." nm
+    do tprintfn "Getting response stream for %s..." nm
     let stream = resp.GetResponseStream()
 
-    do printfn "Reading response for %s..." nm
+    do tprintfn "Reading response for %s..." nm
     let reader = new StreamReader(stream)
     let! html = reader.ReadToEndAsync() |> Async.AwaitTask
 
-    do printfn "Read %d characters for %s..." html.Length nm
-    
+    do tprintfn "Read %d characters for %s..." html.Length nm
   }
+
+let fetchAsync' (nm, url:string) =
+  async.Delay(fun() ->
+      do tprintfn "Creating request for %s..." nm
+      let req = WebRequest.Create(url)
+      async.Bind(req.AsyncGetResponse(), (fun resp -> 
+        do tprintfn "Getting response stream for %s..." nm
+        let stream = resp.GetResponseStream()
+        do tprintfn "Reading response for %s..." nm
+        let reader = new StreamReader(stream)
+        async.Bind(reader.ReadToEndAsync() |> Async.AwaitTask, (fun html -> 
+          do tprintfn "Read %d characters for %s..." html.Length nm
+          async.Return html
+          )
+        ) 
+        ) 
+      ) 
+    ) 
+
+let fetchAsync''(nm, url:string) =
+  async{
+    do tprintfn "Creating request for %s..." nm
+    let req = WebRequest.Create(url)
+
+    let! resp = req.AsyncGetResponse()
+
+    do tprintfn "Getting response stream for %s..." nm
+    let stream = resp.GetResponseStream()
+
+    do tprintfn "Reading response for %s..." nm
+    let reader = new StreamReader(stream)
+    let! html = reader.ReadToEndAsync() |> Async.AwaitTask
+
+    do tprintfn "Read %d characters for %s..." html.Length nm
+    return html
+  }
+    
 
 for nm, url in links do
   fetchAsync (nm, url) |> Async.Start
@@ -33,3 +73,27 @@ for nm, url in links do
 links 
 |> List.toArray 
 |> Array.map (fun (nm, url) ->  fetchAsync (nm, url) |> Async.Start)
+
+fetchAsync''("tachyus", "http://www.tachyus.com") |> Async.RunSynchronously
+printfn "hello"
+
+[for nm, url in links -> fetchAsync'(nm, url)]
+|> Async.Parallel 
+|> Async.Ignore
+|> Async.RunSynchronously
+
+[for nm, url in links -> fetchAsync(nm, url)]
+|> Async.Parallel
+|> Async.RunSynchronously
+
+open System.Threading
+ThreadPool.QueueUserWorkItem(fun _ -> printf "Hello") |> ignore
+
+
+let doSomething something =
+  do something
+  // or
+  //let () = something
+  //()
+
+doSomething (printfn "test")
