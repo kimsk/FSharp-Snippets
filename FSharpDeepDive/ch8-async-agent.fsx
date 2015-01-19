7d0f316f44a9699dd96ff3ecf1f418953b5a5847
Karlkim Suwamongkol <contact@karlk.im>
2015-01-18 19:13:36 -0600

    downloadAgent code

---
+++ FSharpDeepDive/ch8-async-agent.fsx
@@ -0,0 +1,40 @@
+﻿open System.IO
+open System.Net
+open Microsoft.FSharp.Control
+
+let doWebRequest (url:string) methd transformer =
+  async {
+    let request = WebRequest.Create(url, Method = methd)
+    use! response = request.AsyncGetResponse()
+    return! transformer (response.GetResponseStream())
+  }
+
+let readStreamAsString (stream:Stream) =
+  async {
+    use streamReader = new StreamReader(stream)
+    return! streamReader.ReadToEndAsync() |> Async.AwaitTask
+  }
+  
+type Agent<'a> = MailboxProcessor<'a>
+
+type Request = | Get of string * AsyncReplyChannel<string>
+
+let downloadAgent =
+  Agent<_>.Start(fun inbox ->
+    let rec loop (cache: Map<string, string>) =
+      async{
+        let! msg = inbox.Receive()
+        match msg with
+        | Get(url, reply) ->
+          match cache.TryFind(url) with
+          | Some (result) ->
+              reply.Reply(result)
+              return! loop cache
+          | None ->
+              let! result = doWebRequest url "GET" readStreamAsString
+              reply.Reply(result)
+              return! loop (Map.add url result cache)
+      }
+
+    loop Map.empty
+  )
