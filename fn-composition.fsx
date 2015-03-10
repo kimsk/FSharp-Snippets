0352bb742bf6bb7ae01a09f5affb8ab58d6e25aa
Karlkim Suwamongkol <contact@karlk.im>
2015-03-10 14:00:04 -0700

    F# fn composition

---
+++ fn-composition.fsx
@@ -0,0 +1,13 @@
+﻿//http://blogs.msdn.com/b/chrsmith/archive/2008/06/14/function-composition.aspx
+let negate x = x * -1
+let sqr x = x * x
+let print x = printfn "The number is %d" x
+
+let sqr_negate_print = sqr >> negate >> print
+
+10
+|> sqr
+|> negate
+|> print
+
+10 |> sqr_negate_print
