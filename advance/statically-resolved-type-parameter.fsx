9cb6c5446b5fbdca85ea59520670b5c3cc4df7c9
kimsk <contact@karlk.im>
2015-04-26 13:50:01 -0700

    statically resolved type/duck-typing

---
+++ advance/statically-resolved-type-parameter.fsx
@@ -0,0 +1,41 @@
+﻿//  http://weblogs.asp.net/podwysocki/f-duck-typing-and-structural-typing
+//  https://msdn.microsoft.com/en-us/library/dd548046.aspx
+
+type IAnimal = abstract member Roar:unit -> string
+
+let dog = { new IAnimal with member x.Roar() = "DEE" }
+
+type Cat() = member this.Roar() = "CEE"
+
+dog.Roar() |> printfn "%A"
+
+let Roar (animal:IAnimal) = animal.Roar()
+
+dog |> Roar  |> printfn "%A"
+
+let roar2 (animal: ^A when ^A:> IAnimal ):string =
+    animal.Roar()
+
+roar2 dog |> printfn "%A"
+
+let cat = Cat()
+cat.Roar()
+
+//roar2 cat
+
+let inline roar3 (animal: ^A when ^A: (member Roar:unit -> string)):string =
+   (^A:(member Roar:unit->string) animal)
+   //animal
+
+let inline roar4 animal = (^A: (member Roar:unit -> string) animal)
+
+
+
+dog |> roar3 |> printfn "%A"
+cat |> roar3 |> printfn "%A"
+dog |> roar4 |> printfn "%A"
+cat |> roar4 |> printfn "%A"
+   
+  
+   
+
